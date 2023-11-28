646f9228eeab1223813fd4585fa53e69a7b4b5ce
Thalles Rafael <thallesrafaell@outlook.com>
2023-11-28 18:15:38 -0300

    Commit MD5 Tarefa Primeiro projeto

---
+++ mod5/PrimeiroProjeto/src/br/com/trafael/PrimeiraClasse.java
@@ -0,0 +1,8 @@
+package br.com.trafael;
+
+public class PrimeiraClasse {
+    public static void main(String[] args){
+        String nome = "Thalles Rafael";
+        System.out.print("Hello " + nome);
+    }
+}
