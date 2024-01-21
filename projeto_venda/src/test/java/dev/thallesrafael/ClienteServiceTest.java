32809ff033edcbdc7708b5f27364a16fee401ba5
Thalles Rafael <thallesrafaell@outlook.com>
2024-01-20 23:46:40 -0300

    Commit Incial

---
+++ projeto_venda/src/test/java/dev/thallesrafael/ClienteServiceTest.java
@@ -0,0 +1,13 @@
+package test.java.dev.thallesrafael;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class ClienteServiceTest {
+
+    @Test
+    public void salvarTeste(){
+        String nome = "Thalles";
+        Assert.assertEquals("Thalles", nome);
+    }
+}
